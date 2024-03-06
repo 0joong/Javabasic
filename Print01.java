90648b5b7a13d527ef7ba74a37a021fba1d73895
0joong <162318623+0joong@users.noreply.github.com>
2024-03-06 17:36:20 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,13 @@
+package sec04;
+
+public class Print01 {
+	public static void main(String[] args) {
+		String myName = "홍길동";
+		int myage = 20;
+		System.out.println("이름: "+myName);
+		System.out.println("나이: "+myage);
+		System.out.println();
+		
+		System.out.print("이름: "+myName +"         나이: "+myage);
+	}
+}
